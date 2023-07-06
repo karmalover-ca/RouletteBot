6056bed748ab8607095029e0536714b560855c47
karmalover-ca <liamphone0@gmail.com>
2023-07-06 03:55:53 -0400

    adds example.start.sh, and removed mcdata import in ping_command

---
+++ example.start.sh
@@ -0,0 +1,5 @@
+export BOT_TOKEN="botToken"
+export APPLICATION_ID="botApplicationID"
+export DEV_SERVER="production"
+
+yarn dev
--- src/commands/ping_command.ts
+++ src/commands/ping_command.ts
@@ -1,9 +1,6 @@
 import { ChatInputCommandInteraction } from "discord.js";
 import { LOGGER } from "../constants";
 import BaseCommand from "./base_command";
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import mcdata from "mcdata";
 
 class PingCommand extends BaseCommand {
     constructor() {
